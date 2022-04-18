9399ed70aa953e86485aaca6798c2d805ba1ce2c
kcharyulu <k.charyulu@gmail.com>
2022-04-18 16:29:45 +0530

    first commit

---
+++ src/test/java/kcpAcademy/MavenJava/AppiumTest.java
@@ -0,0 +1,17 @@
+package kcpAcademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAndroidApp()
+	{
+		System.out.println("NativeAndroidApp");
+	}
+	@Test
+	public void iOSApp()
+	{
+		System.out.println("iOSApps");
+	}
+
+}
---
+++ src/test/java/kcpAcademy/MavenJava/RESTAPITest.java
@@ -0,0 +1,17 @@
+package kcpAcademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");	
+	}
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
